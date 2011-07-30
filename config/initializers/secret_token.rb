06d7a8b384de6289308f05b17541c7b014d8be16
Gervais <misterloac@gmail.com>
2011-07-30 04:08:35 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cantine::Application.config.secret_token = '741a971d49d5e398c027032cc3fb19f62068f83d94c0cd9d588f7cf55c3058bb54a79ea28bbc7b05f91a2e9b21e4c7fcd3c69033eb41bc0d36afb9d333b7e52b'
